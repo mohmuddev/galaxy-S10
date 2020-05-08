42a297da6cb7cae605fc3f701cc7b21fa4f77e68
Mohamud Ali <info@mohamud.dev>
2020-05-08 17:01:51 +0300

    Final commit

--- index.js
+++ index.js
@@ -37,9 +37,15 @@ document.getElementById("date").innerHTML = formatted_date;
 // Selection of HTML Elememtns
 const LockScreen = document.querySelector(".lock-screen");
 const HomeScreen = document.querySelector(".home");
+const PowerButton = document.querySelector(".btn-3");
 
 //Touch move event Listner
 LockScreen.addEventListener("click", function () {
   LockScreen.classList.add("hide-lockScreen");
   HomeScreen.classList.add("show-homeScreen");
+});
+
+PowerButton.addEventListener("click", function () {
+  LockScreen.classList.remove("hide-lockScreen");
+  HomeScreen.classList.remove("show-homeScreen");
 });
